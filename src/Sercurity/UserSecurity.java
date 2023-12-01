fc6b4319c35d58081bef91ace256ad64a22b0bc6
Cristiano0499.silva <cristiano0499.silva@gmail.com>
2023-12-01 17:39:30 -0500

    fixed file structure

--- src/Sercurity/UserSecurity.java
+++ src/Sercurity/UserSecurity.java
@@ -1,2 +1,4 @@
+package Sercurity;
+
 public class UserSecurity {
 }
--- src/domain/User.java
+++ src/domain/User.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class User {
     private String username;
     private String password;
+    private String location;
     private List<Activity> activities;
 
     public User(String username, String password) {
@@ -44,6 +45,21 @@ public class User {
     }
 
     // Additional methods as needed
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
 
 
