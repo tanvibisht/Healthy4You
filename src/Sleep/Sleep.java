bfa06d4a29a99d982f890781b6752badf90127b9
Tanvi  Bisht <tanvibisht@Tanvis-MacBook-Pro.local>
2023-12-03 18:56:56 -0500

    added sleep usecase

--- src/Sleep/Sleep.java
+++ src/Sleep/Sleep.java
@@ -1,75 +1,72 @@
 package Sleep;
 
-import javax.swing.*;
-import java.awt.event.*;
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Sleep extends JFrame {
-    private JTextField hourField, minuteField, usernameField;
-    private JButton submitButton, clearButton;
-    private static final String FILE_PATH = "src/sleep.txt";
+public class Sleep {
+    private Map<String, Map<String, Double>> userSleepData; // Map of users to their sleep data map
+    private static final String SLEEP_FILE_NAME = "src/sleep.txt";
+    private String currentUsername;
 
     public Sleep() {
-        createUI();
+        userSleepData = new HashMap<>();
+        currentUsername = readCurrentUsername();
+        loadSleepData();
     }
 
-    private void createUI() {
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-
-        usernameField = new JTextField(20);
-        hourField = new JTextField(2);
-        minuteField = new JTextField(2);
-        submitButton = new JButton("Submit");
-        clearButton = new JButton("Clear");
-
-        submitButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                writeSleepData();
-            }
-        });
-
-        clearButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                clearSleepData();
-            }
-        });
-
-        add(new JLabel("Username:"));
-        add(usernameField);
-        add(new JLabel("Hours:"));
-        add(hourField);
-        add(new JLabel("Minutes:"));
-        add(minuteField);
-        add(submitButton);
-        add(clearButton);
-
-        pack();
-        setVisible(true);
+    public void addSleepData(String day, double hours) {
+        Map<String, Double> sleepData = userSleepData.getOrDefault(currentUsername, new HashMap<>());
+        sleepData.put(day, hours);
+        userSleepData.put(currentUsername, sleepData);
+        saveSleepData();
     }
 
-    private void writeSleepData() {
-        String username = usernameField.getText();
-        String hours = hourField.getText();
-        String minutes = minuteField.getText();
+    public void clearSleepData() {
+        userSleepData.put(currentUsername, new HashMap<>()); // Clearing data for the current user
+        saveSleepData();
+    }
 
-        try (FileWriter fw = new FileWriter(FILE_PATH, true);
-             BufferedWriter bw = new BufferedWriter(fw);
-             PrintWriter out = new PrintWriter(bw)) {
-            out.println(username + ": " + hours + " hours " + minutes + " minutes");
+    private void loadSleepData() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(SLEEP_FILE_NAME))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 3) {
+                    String username = parts[0];
+                    String day = parts[1];
+                    double hours = Double.parseDouble(parts[2]);
+                    userSleepData.computeIfAbsent(username, k -> new HashMap<>()).put(day, hours);
+                }
+            }
         } catch (IOException e) {
-            System.out.println("An error occurred: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    private void clearSleepData() {
-        hourField.setText("");
-        minuteField.setText("");
+    private void saveSleepData() {
+        try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(SLEEP_FILE_NAME)))) {
+            for (Map.Entry<String, Map<String, Double>> userEntry : userSleepData.entrySet()) {
+                String username = userEntry.getKey();
+                for (Map.Entry<String, Double> sleepEntry : userEntry.getValue().entrySet()) {
+                    out.println(username + "," + sleepEntry.getKey() + "," + sleepEntry.getValue());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void main(String[] args) {
-        new Sleep();
+    String readCurrentUsername() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("currentusername.txt"))) {
+            return reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null; // Handle appropriately
+        }
+    }
+
+    public Map<String, Double> getUserSleepData() {
+        return userSleepData.getOrDefault(currentUsername, new HashMap<>());
     }
 }
